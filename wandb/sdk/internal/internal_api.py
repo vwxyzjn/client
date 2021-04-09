b91d29bba45b56c756843b8e3f473be897b92285
Fariz Rahman <farizrahman4u@gmail.com>
2021-04-09 23:01:49 +0530

    fixz

--- wandb/sdk/internal/internal_api.py
+++ wandb/sdk/internal/internal_api.py
@@ -1942,7 +1942,7 @@ class Api(object):
             raise Exception("Cannot resume %s sweep." % curr_state.lower())
         elif state == "PAUSED" and curr_state not in ("PAUSED", "RUNNING"):
             raise Exception("Cannot pause %s sweep." % curr_state.lower())
-        elif state not in ("RUNNING", "PAUSED"):
+        elif curr_state not in ("RUNNING", "PAUSED"):
             raise Exception("Sweep already %s." % curr_state.lower())
         sweep_id = s["id"]
         mutation = gql(
